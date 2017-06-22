f04460021230d94fc8dcd8fc902b6874789a00b3
Дарья Высокович <darya.vysokovich@elatesoftware.com>
2017-06-22 16:52:36 +0300

    fix bugs design terminal and menu

--- app/src/main/java/com/elatesoftware/grandcapital/views/items/chart/ViewInfoHelper.java
+++ app/src/main/java/com/elatesoftware/grandcapital/views/items/chart/ViewInfoHelper.java
@@ -36,6 +36,10 @@ public class ViewInfoHelper {
         rlChart = rl;
         mOpenDealingView = LayoutInflater.from(mContext).inflate(R.layout.incl_label_open_dealing, null);
         mCloseDealingView = LayoutInflater.from(mContext).inflate(R.layout.incl_label_close_dealing, null);
+
+        mOpenDealingView.setBackgroundColor(ConventImage.getColorWithAlpha(GrandCapitalApplication.getAppContext().getResources().getColor(R.color.colorPrimaryDark), 0.7f));
+        mCloseDealingView.setBackgroundColor(ConventImage.getColorWithAlpha(GrandCapitalApplication.getAppContext().getResources().getColor(R.color.dealingOpenOrCloseOrderColor), 0.7f));
+
         mOpenDealingView.setTag(TAG_OPEN_DEALING);
         mCloseDealingView.setTag(TAG_CLOSE_DEALING);
     }
